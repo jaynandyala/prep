9d743c9c9c6b5c952fd0abc9e8cb363fc8dc001a
Jaynandyala <jay@optobizz.com>
2020-11-30 13:26:48 +0530

    q1 solution

---
+++ .history/q1_20201130131945.js
@@ -0,0 +1,38 @@
+
+function findTargetWhile(arr, sumToFind){
+    let result = [];
+    let currentIteration = 0;
+    let valueFound = false;
+    while(currentIteration < arr.length && !valueFound){
+        let innerIteration = 0;
+        while(innerIteration < arr.length && !valueFound){
+            const i = arr[currentIteration];
+            const j = arr[innerIteration];
+            if(j != i){
+                if (i+j == sumToFind) {
+                    result = [i,j];
+                    valueFound = true;
+                }
+            }
+            if(valueFound){
+                break;
+            }
+            innerIteration++;
+        }
+        if(valueFound){
+            break;
+        }
+        currentIteration++;
+    }
+    console.log(result);
+}
+
+const  inputArr1 = [0,1,2,3,4,5];
+let targetSum1 = 8;
+
+findTargetWhile(inputArr1, targetSum1);
+
+const  inputArr2 = [1,11,25,32,99,86,14];
+let targetSum2 = 8;
+
+findTargetWhile(inputArr2, targetSum2);
---
+++ q1.js
@@ -0,0 +1,47 @@
+
+function checkIfArrayIsUnique (arr){
+    //works from ES2015
+    return (new Set(arr)).size === arr.length;
+}
+
+function findTargetWhile(arr, sumToFind){
+    if(!checkIfArrayIsUnique(arr)){
+        console.log("Array not valid. it contains duplicate values");
+        return;
+    }
+    let result = [];
+    let currentIteration = 0;
+    let valueFound = false;
+    while(currentIteration < arr.length && !valueFound){
+        let innerIteration = 0;
+        while(innerIteration < arr.length && !valueFound){
+            const i = arr[currentIteration];
+            const j = arr[innerIteration];
+            if(j != i){
+                if (i+j == sumToFind) {
+                    result = [i,j];
+                    valueFound = true;
+                }
+            }
+            if(valueFound){
+                break;
+            }
+            innerIteration++;
+        }
+        if(valueFound){
+            break;
+        }
+        currentIteration++;
+    }
+    console.log(result);
+}
+
+const  inputArr1 = [0,1,2,3,4,5];
+let targetSum1 = 8;
+
+findTargetWhile(inputArr1, targetSum1);
+
+const  inputArr2 = [1,1,25,32,99,86,14];
+let targetSum2 = 100;
+
+findTargetWhile(inputArr2, targetSum2);
